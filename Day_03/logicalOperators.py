900536565877b2d07533960ae3886e76165b26c2
manojreddykatta <manojkatta97@gmail.com>
2022-08-13 23:22:47 +0530

    logical operators and or not

---
+++ Day_03/logicalOperators.py
@@ -0,0 +1,9 @@
+# and returns true if both statements are true
+# or returns true if one of the statement is true
+# not revere the result, results false if the result is true
+
+
+print(3 > 2 and 6 > 3)
+print(3 > 2 or 1 > 3)
+print(1 > 2 and 1 > 3)
+print(not True)
