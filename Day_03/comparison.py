0fdd2b5cb6ea8a423d32c14f3be28f3f15a81c3f
manojreddykatta <manojkatta97@gmail.com>
2022-08-13 23:13:24 +0530

    comparison operators

---
+++ Day_03/comparison.py
@@ -0,0 +1,36 @@
+# # comparison operators in python
+# == equal
+# != not equal
+# > greater than
+# <less Than
+# >= greater than or equal to
+# <= less than or equal
+
+
+print(3 > 2)
+print(3 >= 2)
+print(3 < 2)
+print(2 < 3)
+print(2 <= 3)
+print(3 == 2)
+print(3 != 2)
+
+print(len('mango') == len('guava'))
+
+# is : returns true if both values are same objects (x is y)
+print('1 is 1', 1 is 1)
+
+# is not : returns true if both values are not same objects (x is y)
+
+print('1 is not 2', 1 is 2)
+
+# in: Returns True if the queried list contains a certain item(x in y)
+
+print('M in manoj', 'M' in 'Manoj')
+print('M in manoj', 'M' in 'manoj')
+print('coding' in 'coding for all')
+
+# not in: Returns True if the queried list doesn't have a certain item(x in y)
+
+print('C not in manoj', 'C' not in 'Manoj')
+print('4 is 2**2:', 4 is 2**2)
